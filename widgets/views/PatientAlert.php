7266bab3c4d7cd05f5b3f1803e068563a2468886
Mike Smith <mike.smith@openeyes.org.uk>
2014-10-07 09:29:40 +0100

    + [OE-5093] user permission check for ticket banner display

--- widgets/views/PatientAlert.php
+++ widgets/views/PatientAlert.php
@@ -17,7 +17,7 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
 ?>
-<?php if (count($tickets)) { ?>
+<?php if (count($tickets) && Yii::app()->user->checkAccess('OprnViewClinical')) { ?>
 	<div class="row">
 		<div class="large-12 column">
 			<?php foreach ($tickets as $ticket) {
